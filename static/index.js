3c3a850d1ca531cd5b748070dcdbc9393c4b5d16
mghswe <98584290+mghswe@users.noreply.github.com>
2022-01-28 18:12:54 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '菜狗？';
+        if (cps <= 8) return 'cys';
+        if (cps <= 10)  return 'love';
+        if (cps <= 15) return 'lyl';
+        return 'forever';
     }
 
     function toStr(obj) {
